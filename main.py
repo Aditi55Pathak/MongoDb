c29deebb3c1f2bbfdd22c1042e47595c3a7956e0
Aditi Pathak <Aditi55Pathak>
2024-04-12 21:25:22 +0530

    learning mongo

--- main.py
+++ main.py
@@ -11,5 +11,11 @@ if __name__=="__main__":
     # collection.insert_one(dict)
 
     insertHere=[{"Name":"Aditi Pathak","Age":20,"Dream":"Will buy & BHK"},
-                ]
-
+                {"Name":"Ishita Chauhan","Age":21,"Dream":"Will marry Mrinal"},
+                {"Name":"Avani Chaturvedi","Age":21,"Dream":"Will do AI/Ml"},
+                {"Name":"Heli Bhatt","Age":19,"Dream":"Want a Chava"},
+                {"Name":"Pranjal Patel","Age":19,"Dream":"I want Aarsh Aatrey"},
+                {"Name":"Puja Mahavadiya","Age":22,"Dream":"Jemmy is my dream Husband"},
+                {"Name":"Mahek Gor","Age":21,"Dream":"I am an archeoptrix"}]
+    
+    collection.insert_many(insertHere)
